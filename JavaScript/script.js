498795090f6820d461a7c2e6ffecc92fbfaac07b
Samima-Akter-Tithy <tithysamima789@gmail.com>
2022-10-07 10:32:33 +0600

    Running Project

---
+++ JavaScript/script.js
@@ -0,0 +1,14 @@
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function(){
+	header.classList.toggle ("sticky", window.scrollY > 0)
+});
+
+
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navbar.classList.toggle('active');
+}
